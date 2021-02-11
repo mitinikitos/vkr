5337c6d733d7adb0ead2285098a61d97094b5cf7
mitinikitos <mityushkin.nik.99@gmail.com>
2021-02-11 13:46:46 +0300

    edit entity, add columnDefinition

--- src/main/java/com/example/vkr/ship/model/ShipEngine.java
+++ src/main/java/com/example/vkr/ship/model/ShipEngine.java
@@ -21,65 +21,62 @@ public class ShipEngine {
     @Column(name = "reg_num")
     private int regNum;
     @JsonView(View.REST.class)
-    @OneToOne(fetch = FetchType.LAZY)
-    @PrimaryKeyJoinColumn(name = "reg_num", referencedColumnName = "reg_num")
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "reg_num", referencedColumnName = "reg_num")
     private Ship ship;
 
 //    @JsonView(View.REST.class)
     @Nullable
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "eng_1", referencedColumnName = "id", updatable = false)
+    @JoinColumn(name = "eng_1", referencedColumnName = "id", updatable = false, columnDefinition = "uuid")
     private Engine engine1;
 
 //    @JsonView(View.REST.class)
     @Nullable
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "eng_2", referencedColumnName = "id", updatable = false)
+    @JoinColumn(name = "eng_2", referencedColumnName = "id", updatable = false, columnDefinition = "uuid")
     private Engine engine2;
 
 //    @JsonView(View.REST.class)
     @Nullable
     @ManyToOne(targetEntity = Engine.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "eng_3", referencedColumnName = "id", updatable   = false)
+    @JoinColumn(name = "eng_3", referencedColumnName = "id", updatable   = false, columnDefinition = "uuid")
     private Engine engine3;
 
     @Column(name = "sum_pwr")
     private int sumPwr;
 
-    /**
-     * Create {@link ShipEngine} for the given {@link Ship} and {@link Engine}
-     * @param ship must not be {@literal null}.
-     * @param engine1 can be {@literal null}.
-     * @param engine2 can be {@literal null}.
-     * @param engine3 can be {@literal null}.
-     */
     public ShipEngine(Ship ship, @Nullable Engine engine1, @Nullable Engine engine2, @Nullable Engine engine3) {
         this.regNum = ship.getId();
         this.ship = ship;
         this.engine1 = engine1;
         this.engine2 = engine2;
         this.engine3 = engine3;
-        sumPwr(engine1, engine2, engine3);
+        sumPwr();
     }
 
-    /**
-     * Creates {@link ShipEngine} for the given {@link Ship}
-     * @param ship must not be {@literal null}.
-     */
+
     public ShipEngine(Ship ship) {
         this(ship, null, null, null);
     }
 
-    /**
-     * Set {@link ShipEngine#sumPwr}
-     * @param engine1 can be {@literal null}.
-     * @param engine2 can be {@literal null}.
-     * @param engine3 can be {@literal null}.
-     */
-    private void sumPwr(@Nullable Engine engine1, @Nullable Engine engine2, @Nullable Engine engine3) {
+    private void sumPwr() {
         this.sumPwr = engine1 == null ? 0 : engine1.getPwr() * engine1.getCount();
         this.sumPwr += engine2 == null ? 0 : engine2.getPwr() * engine2.getCount();
         this.sumPwr += engine3 == null ? 0 : engine3.getPwr() * engine3.getCount();
+    }
+
+    private void setEngine1(Engine engine1) {
+        this.engine1 = engine1;
+        sumPwr();
+    }
+    private void setEngine2(Engine engine2) {
+        this.engine2 = engine2;
+        sumPwr();
+    }
+    private void setEngine3(Engine engine3) {
+        this.engine3 = engine3;
+        sumPwr();
     }
 
     @Override
